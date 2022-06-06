e2651aaa638436ebaaeff9cf6a7a8ec853052006
mayankarora <mayank.arora@budbee.com>
2022-06-06 08:18:03 +0200

    Initial commit with successful hibernate connection

---
+++ Readme.md
@@ -0,0 +1,3 @@
+git remote add origin https://github.com/mayank-budbee/mysql-hibernate.git
+git branch -M main
+git push -u origin main
---
+++ src/main/java/com/example/mysqlhibernate/MysqlHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqlhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlHibernateApplication.class, args);
+    }
+
+}
