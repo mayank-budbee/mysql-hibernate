d81de36f44f25a8bcb63c5b29feaa93da38daeee
mayankarora <mayank.arora@budbee.com>
2022-06-07 14:41:41 +0200

    Added a test

--- src/test/java/com/example/mysqlhibernate/MysqlHibernateApplicationTests.java
+++ src/test/java/com/example/mysqlhibernate/MysqlHibernateApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.mysqlhibernate;
 
+import com.example.mysqlhibernate.model.Employee;
+import com.example.mysqlhibernate.service.EmployeeService;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class MysqlHibernateApplicationTests {
 
+    @Autowired
+    private EmployeeService employeeService;
     @Test
-    void contextLoads() {
+    void getAllEmployees() {
+        List<Employee> employees = employeeService.getAllEmployees();
+        log.info("Employee count: {}", employees.size());
+        Assertions.assertEquals(1, employees.size());
     }
 
+    @Test
+    void findEmployeeById() {
+        Employee employee = employeeService.getEmployeeById(1);
+        Assertions.assertEquals("raj", employee.getName());
+    }
 }
